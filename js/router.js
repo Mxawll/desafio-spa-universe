fa8f9375734baf5ff8c4cc9103bb8541b71ebb2a
mxawll <maxsilva9080@gmail.com>
2024-07-09 22:34:43 -0300

    refactor the functions

--- js/router.js
+++ js/router.js
@@ -2,5 +2,20 @@ class Router {
    route(event) {
       event = event || window.event
       event.preventDefault()
+   
+      window.history.pushState({}, "", event.target.href) // captura todo o href do evento alvo e coloca no historico
+   
+      this.handle()
+   }
+
+   handle() {
+      const { pathname } = window.location
+      const route = routes[pathname] || routes[404]
+   
+      fetch(route)
+      .then(data => data.text())
+      .then(html => {
+         document.getElementById("app").innerHTML = html
+      })
    }
 }
